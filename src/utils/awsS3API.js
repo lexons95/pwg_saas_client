8fe5d8e68ac2f190db4a0957dc62552e0a09dc4d
shj <lexons95@hotmail.com>
2020-10-23 18:51:15 +0800

    secure api key

--- src/utils/awsS3API.js
+++ src/utils/awsS3API.js
@@ -1,21 +1,45 @@
 import AWS from 'aws-sdk';
+import ApolloClientAPI from './ApolloClientAPI';
+import gql from 'graphql-tag';
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 const albumBucketName = "mananml";
 const bucketRegion = 'ap-east-1';
-const IdentityPoolId = 'ap-east-1:bcd5bb0f-792b-40fc-b8cb-20632b3453c1';
-const accessKey = 'AKIA2MHRZXWFZKCVXX6S';
-const secretAccessKey = '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';
+// const IdentityPoolId = 'ap-east-1:bcd5bb0f-792b-40fc-b8cb-20632b3453c1';
+const accessKey = process.env.AWSS3_ACCESSKEY;
+const secretAccessKey = process.env.AWSS3_SECRETACCESSKEY;
+  
+const AWS_GET_SIGNED_URL = gql`
+  query getS3SignedUrl($bucketName: String!, $Key: String!, $ContentType: String!) {
+    getS3SignedUrl(bucketName: $bucketName, Key: $Key, ContentType: $ContentType) {
+      success
+      message
+      data
+    }
+  }
+`;
 
-const awsS3API = async () => {
+const AWS_DELETE_ONE = gql`
+  mutation s3DeleteOne($bucketName: String!, $Key: String!) {
+    s3DeleteOne(bucketName: $bucketName, Key: $Key) {
+      success
+      message
+      data
+    }
+  }
+`;
+
+
+const awsS3API = (bucketName) => {
   // AWS.config.region = bucketRegion; 
   // AWS.config.credentials = new AWS.CognitoIdentityCredentials({
   //   IdentityPoolId: IdentityPoolId
   // });
+  const apolloClient = ApolloClientAPI();
+
   AWS.config.update({
     region: bucketRegion,
-    // credentials: new AWS.CognitoIdentityCredentials({
-    //   IdentityPoolId: IdentityPoolId
-    // }),
     accessKeyId: accessKey,
     secretAccessKey: secretAccessKey
   });
@@ -23,67 +47,54 @@ const awsS3API = async () => {
   let s3 = new AWS.S3({
     region: bucketRegion,
     apiVersion: "2006-03-01",
-    params: { Bucket: albumBucketName },
-    //accessKeyId: accessKey,
-    //secretAccessKey: secretAccessKey,
+    //params: { Bucket: albumBucketName },
     maxRetries: 3
   });
 
   return {
     getImages: async () => {
       return new Promise((resolve, reject) => {
-        s3.listBuckets(function(err, data) {
+        var params = {
+          Bucket: bucketName, 
+          //MaxKeys: 2
+         };
+         s3.listObjects(params, function(err, data) {
           if (err) {
             console.log("Error", err);
             reject(err);
 
           } else {
-            console.log("Success", data.Buckets);
+            console.log("Success", data);
             resolve(data)
           }
-        });
-        // s3.listObjects({
-        //   Bucket : albumBucketName,
-        // }, function(err, data) {
-        //   if (err) {
-        //     console.log("Error", err);
-        //     reject(err);
-        //   } else {
-        //     console.log("Success", data);
-        //     resolve(data)
-        //   }
-        // });
-      })
-      
-    },
-    uploadOne: async (photoKey, file) => {
-      return new Promise((resolve, reject) => {
-        var upload = new AWS.S3.ManagedUpload({
-          params: {
-            Bucket: 'mananml',
-            Key: photoKey,
-            Body: file,
-            ACL: "public-read"
-          }
-        });
-        console.log('file',file)
-        console.log('upload',upload)
-        var promise = upload.promise();
-        promise.then(
-          function(data) {
-            alert("Successfully uploaded photo.");
-            resolve(data)
-          },
-          function(err) {
-            alert("There was an error uploading your photo: ", err.message);
-            reject(err)
-          }
-        );
+        })
       })
     },
-    delete: () => {
 
-    }
+    getSignedUrl: async (Key, ContentType) => {
+      return new Promise((resolve, reject) => {
+        apolloClient.query(AWS_GET_SIGNED_URL,{
+          bucketName, Key, ContentType
+        })
+        .then(result=>resolve(result))
+        .catch(err=>{
+          console.log(err);
+          reject(err)
+        });
+      })
+    },
+    deleteOne: async (Key) => {
+      return new Promise((resolve, reject) => {
+        apolloClient.mutation(AWS_DELETE_ONE,{
+          bucketName, Key
+        })
+        .then(result=>resolve(result))
+        .catch(err=>{
+          console.log(err);
+          reject(err)
+        });
+      })
+    },
   }
 
 }
